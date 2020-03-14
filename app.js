3c5320d5c7d65babc2962cdba62fd60db8aba16f
Zachary Wilson <zaxmaxpro@Zacharys-MacBook-Pro.local>
2020-03-13 22:22:38 -0500

    refactored jQuery

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('jQuery init');
     //attach a click event listener to all the img tags when the document is ready
-        $('img').click(function(){
-            $(this).hide();
-        });
-
-$( "img" ).hover(
-    function() {
-        $( this ).attr("src", 'img/two.jpg' );
-        console.log('hover');
-    },
-    function() {
-        $( this ).attr( "src", $(this.attr( "src")));
-        console.log('unhover');
-    }
-    );
+    $('img').click(function () {
+        $(this).hide();
     });
+
+    var imgSourceSwap = function () {
+        var $this = $(this);
+        var newSource = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', newSource);
+    }
+
+    $(function () {
+        $('img[data-alt-src]').each(function () {
+            new Image().src = $(this).data('alt-src');
+        }).hover(imgSourceSwap, imgSourceSwap);
+    });
+});
